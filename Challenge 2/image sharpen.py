9f756c0ec2825fad8d25e5e43b602922ce682dc1
Ong Chung Xian <s2251448@sms.ed.ac.uk>
2022-07-25 11:19:59 +0100

    Updated image processing source code

--- Challenge 2/image sharpen.py
+++ Challenge 2/image sharpen.py
@@ -1,9 +1,20 @@
 import cv2
 import numpy as np
 
-image = cv2.imread('1.jpg')
-sharpen_kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
-sharpen = cv2.filter2D(image, -1, sharpen_kernel)
+image = cv2.imread('4.jpg')
+gauss_kernel = -1/256 * np.array([[1, 4, 6, 4, 1],
+                                  [4, 16, 24, 16, 4],
+                                  [6, 24, -476, 24, 6],
+                                  [4, 16, 24, 16, 4],
+                                  [1, 4, 6, 4, 1]]
 
-cv2.imshow('sharpen', sharpen)
-cv2.waitKey()
+)
+sharp_kernel = np.array([[0,-1,0], 
+                         [-1,5,-1],
+                         [0,-1,0]])
+sharp_kernel = 2/3 * sharp_kernel
+gauss = cv2.filter2D(image, -1, gauss_kernel)
+sharpened = cv2.filter2D(gauss, -1, sharp_kernel) 
+cv2.imshow('Image Sharpening', sharpened)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
--- Challenge 3/data cleaning.py
+++ Challenge 3/data cleaning.py
@@ -1,11 +1,46 @@
 import re 
+import pandas as pd
+
+def checknamesyntax(name):
+    output = re.sub(r"[^a-zA-Z ]", "", name)
+
+    return output
 
 def checkmailsyntax(email):
     output = re.sub(r"[^a-zA-Z0-9@\.]+", "", email)
 
-    
-    print(output)
-    
+    return output
+
+def checkphonesyntax(phone):
+    output = re.sub(r"[^0-9\-]+", "", phone)
+
+    return output
+
+def checklicenseseq(i):
+    l = list(i)
+    if l[0].isalpha() == False:
+        e = l.pop(0)
+        l.append(e)
+    output = ''.join(l)
+    return output
 
 
-s = checkmailsyntax("Emil[y_Kli#%ne36@mail.com")
+def checklicensesyntax(l):
+    output = re.sub(r"[^A-Z0-9\-]+", "", l)
+
+    output = checklicenseseq(output)
+    return output
+
+loyalty = pd.read_csv("test_dataset/loyalty.csv")
+transaction = pd.read_csv("test_dataset/transactions.csv")
+
+for i in loyalty.index:
+    loyalty.loc[i, 'name'] = checknamesyntax(loyalty.loc[i, 'name'])
+    loyalty.loc[i, 'city'] = checknamesyntax(loyalty.loc[i, 'city'])
+    loyalty.loc[i, 'phone-number'] = checkphonesyntax(loyalty.loc[i, 'phone-number'])
+    loyalty.loc[i, 'license-plate'] = checklicensesyntax(loyalty.loc[i, 'license-plate'])
+    loyalty.loc[i, 'email'] = checkmailsyntax(loyalty.loc[i, 'email'])
+merged = pd.merge(loyalty, transaction, on='id')
+#merged2 = pd.concat([merged, transaction['Amount']])
+print(merged)
+
